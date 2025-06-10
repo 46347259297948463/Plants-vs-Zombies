0b58a2b48442af48908aeb12bad39142142a5234
Yasamin <yasamin.vazifehdan31@gmail.com>
2025-06-10 17:25:22 +0330

    Day two end the graphic of pickIcon page DATE : 3.20

--- src/controller/PickIcon.java
+++ src/controller/PickIcon.java
@@ -1,5 +1,88 @@
 package controller;
 
-public class PickIcon {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PickIcon implements Initializable{
+
+        @FXML
+        private Button cherrybombSelecter;
+
+        @FXML
+        private Button cherrybombSelecter1;
+
+        @FXML
+        private Button cherrybombSelecter2;
+
+        @FXML
+        private Button cherrybombSelecter3;
+
+        @FXML
+        private Button peashooterSelecter;
+
+        @FXML
+        private Button peashooterSelecter2;
+
+        @FXML
+        private Button peashooterSelecter3;
+
+        @FXML
+        private Button plant1BTN;
+
+        @FXML
+        private Button plant2BTN;
+
+        @FXML
+        private Button plant3BTN;
+
+        @FXML
+        private Button plant4BTN;
+
+        @FXML
+        private Button plant5BTN;
+
+        @FXML
+        private Button plant6BTN;
+
+        @FXML
+        private Button plant7BTN;
+
+        @FXML
+        private Button playBTN;
+
+        @FXML
+        private Button repeaterSelecter;
+
+        @FXML
+        private Button snowshooterSelecter;
+
+        @FXML
+        private Button sunflowerSelecter;
+
+        @FXML
+        private Button sunflowerSelecter2;
+
+        @FXML
+        private Button sunflowerSelecter3;
+
+        @FXML
+        private Button wallnutSelecter;
+
+        @FXML
+        private Button wallnutSelecter1;
+
+        @FXML
+        private Button wallnutSelecter2;
+
+        @FXML
+        private Button wallnutSelecter3;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
 }
