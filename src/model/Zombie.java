ebedf0283f226136598f8bf6e98b44f5fba089d9
dell <mina.j138559@gmail.com>
2025-06-17 16:14:21 +0330

    update zombie classes

--- src/model/Zombie.java
+++ src/model/Zombie.java
@@ -2,6 +2,9 @@ package model;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
 
 public class Zombie {
     private double row;
@@ -10,6 +13,8 @@ public class Zombie {
     private int speed = 4;
     private ImageView image;
     private boolean attack = false;
+    private Timeline moveTimeline;
+    private boolean isremoved = false;
 
     public Zombie(int x, int y){
         this.row = x;
@@ -19,10 +24,39 @@ public class Zombie {
         setImage(imageV);
     }
 
+    public void startmove(){
+        if(moveTimeline == null){
+            moveTimeline = new Timeline(new KeyFrame(Duration.millis(100), event -> move()));
+            moveTimeline.setCycleCount(Timeline.INDEFINITE);
+        }
+    }
+
+    public void stopmove(){
+        if(moveTimeline != null){
+            moveTimeline.stop();
+        }
+    }
+
     public void move(){
         if (!attack){
             row -= speed;
+            image.setLayoutX(row);
         }
+    }
+
+    public void update(){
+        if(isDead()){
+            stopmove();
+            isremoved = true;
+            //باید روی زامبی ها فور بزنیم و اگر این بولین درست بود از روت عکس آن رو ریموو کنیم
+            return;
+        }
+        if(row <= 250){
+            //بازیکن باخته و زامبی به انتهای زمین رسیده است
+            //پیشنهاد من این است یک بولین تعریف شود که مشخص کند بازی تمام شده
+            //گفتم هم فکری کنیم بعد تعییرات این بخش رو بدم
+        }
+        image.setLayoutX(row);
     }
 
     public void takeDamage(int damage){
@@ -31,10 +65,6 @@ public class Zombie {
 
     public boolean isDead(){
         return HP <= 0;
-    }
-
-    public void update(){
-
     }
 
     public void setSpeed(int speed) {
