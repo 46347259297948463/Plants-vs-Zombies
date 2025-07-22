6bf8c3b106f6e8ce09a062378cbc2ea024d0a3cd
dell <mina.j138559@gmail.com>
2025-07-22 10:59:44 +0330

    create puff shroom and puff Bullet and doom shroom

---
+++ src/model/PuffBullet.java
@@ -0,0 +1,25 @@
+package model;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.awt.*;
+
+public class PuffBullet extends Bullet{
+    public PuffBullet(double row, double column) {
+            super(row, column);
+    }
+
+    @Override
+    protected void setting(){
+        Image image = new Image(getClass().getResource("/view/images/purple bullet.png").toString());
+        ImageView imageV = new ImageView(image);
+        imageV.setFitHeight(10);
+        imageV.setFitWidth(10);
+        imageV.setLayoutX(this.column);
+        imageV.setLayoutY(this.row);
+        this.imageView = imageV;
+        color = "purple";
+        isSnow = false;
+    }
+}
